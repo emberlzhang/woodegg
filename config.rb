07675a0ea7e9db5e0d2237bb284add4467dbc219
Derek Sivers <derek@sivers.org>
2013-06-04 21:15:29 +1200

    using foundation CSS+SCSS

---
+++ config.rb
@@ -0,0 +1,17 @@
+require 'zurb-foundation'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "scss"
+images_dir = "public/images"
+javascripts_dir = "public/js/libs"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
