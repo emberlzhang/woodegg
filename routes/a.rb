8557f51a4e5b96d4b13d598d96905c6de5dedff9
Derek Sivers <derek@sivers.org>
2013-06-05 21:24:35 +1200

    /proof /thanks @customer, etc

--- routes/a.rb
+++ routes/a.rb
@@ -10,11 +10,31 @@ class WoodEggA < Oth
 
   before Oth::ROUTEREG do
     oth!
+    @customer = @person.customer
   end
-  
+
   get '/' do
-    @pagetitle = 'Wood Egg account'
+    redirect "#{OTH_MAP}/proof" if @customer.nil?
+    @pagetitle = 'account'
     erb :home
+  end
+
+  get '/proof' do
+    @pagetitle = 'book registration'
+    erb :proof
+  end
+
+  post '/proof' do
+    if params[:book].nil? || Book[code: params[:book]].nil? || params[:proof].nil? || params[:proof].empty?
+      redirect "#{OTH_MAP}/proof"
+    end
+    @person.add_userstat(statkey: 'proof-' + params[:book], statvalue: params[:proof], created_at: Date.today)
+    redirect "#{OTH_MAP}/thanks"
+  end
+
+  get '/thanks' do
+    @pagetitle = 'thank you'
+    erb :thanks
   end
 
 end
