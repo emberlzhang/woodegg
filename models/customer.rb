1f4c1380304cad8546a59c706f61213cb92b6111
Derek Sivers <derek@sivers.org>
2013-06-08 12:10:29 +1200

    customer email_first

--- models/customer.rb
+++ models/customer.rb
@@ -3,4 +3,10 @@ class Customer < Sequel::Model(WoodEgg::DB)
   many_to_many :books
   include Persony
 
+  def email_first(opts={})
+    opts[:baseurl] ||= 'https://woodegg.com/a/'
+    opts[:subject] ||= 'your Wood Egg ebook'
+    opts[:message] ||= 'I just created you an account on woodegg.com with your free ebook available to download inside. Enjoy!'
+    person.email_reset_message(opts)
+  end
 end
